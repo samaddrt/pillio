0b46977d9421c88313d5b92c0b2b89c8d534feb5
Samad <samad@example.com>
2026-05-24 23:26:31 +0300

    add python deps

--- replit.nix
+++ replit.nix
@@ -5,5 +5,12 @@
     pkgs.openssl
     pkgs.pkg-config
     pkgs.gnumake
+    pkgs.python311
+    pkgs.python311Packages.pip
+    (pkgs.python311.withPackages (ps: [
+      ps.python-telegram-bot
+      ps.requests
+      ps.python-dotenv
+    ]))
   ];
 }
